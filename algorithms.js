e63ddfaea20f8c0ecd79abc0f32cb64d5634fcb2
halilulloh <codevme@gmail.com>
2023-07-17 18:32:37 +0500

    more

--- algorithms.js
+++ algorithms.js
@@ -10,6 +10,14 @@ function reverse_digits(num) {
 }
 
 // console.log(reverse_digits(12345))
+// res = 5*10=50
+// res = 54
+// res = 54*10=540
+// res = 543
+// res = 543*10=5430
+// res = 5432
+// res = 5432*10=54320
+// res = 54321
 
 /*
 710
@@ -30,11 +38,11 @@ function convert_to_binary(num){
 
     while (num !== 0) {
         res += num % 2
-        num  = Math.floor(num / 2)
+        num = Math.floor(num / 2)
     }
     return [...res].reverse().join('')
 }
-
+// console.log(convert_to_binary(13));
 /* 
 13 % 2 = 1
 res = 1
@@ -42,9 +50,24 @@ res = 1
 6 % 2 = 0
 res = 10
 */
+function convert_to_denary(num){
+    let res = 0
+    num = Number(num)
+    let lastDigit;
+    let base = 1
+    while (num !== 0) {
+        lastDigit = (num % 10)
+        num = Math.floor(num / 10)
 
+        res += lastDigit * base
+        base = base * 2
+    }
+    return res
+}
 
-// console.log(convert_to_binary(50));
+console.log(convert_to_denary(1101));
+
+// console.log(convert_to_binary(13));
 // Here is the factorialization function
 function factorial(num){
 
@@ -90,6 +113,82 @@ function fit_rooks(num){ // Here num is the number of cells of the chess board
 
   return (num * fit_rooks(num - 1))
 }
-console.time('time');
-console.log(fit_rooks(1000));  // The time is compared with recursive and loop = no big difference
-console.timeEnd('time');
+// console.time('time');
+// console.log(fit_rooks(1000));  // The time is compared with recursive and loop = no big difference
+// console.timeEnd('time');
+
+// Check if words are palindrome
+
+function is_palindrome(word){
+    let start = 0
+    let end = word.length - 1
+
+    while(start < end){
+        if(word[start] !== word[end]) return false
+        start += 1
+        end -= 1
+    }
+
+    return true
+}
+
+// console.log(is_palindrome('doniyor'));
+
+function merge_arrays(arr1, arr2) {
+
+    let result = []
+    let i = 0;
+    let j = 0;
+
+    while(i < arr1.length && j < arr2.length){
+        if(arr1[i] < arr2[j]){
+            result.push(arr1[i])
+            i += 1
+        } else {
+            result.push(arr2[j])
+            j += 1
+        }
+
+        while (i < arr1.length){
+            result.push(arr1[i])
+            i += 1
+        }
+        while (j < arr2.length){
+            result.push(arr2[j])
+            j += 1
+        }
+    }
+    return result
+}
+
+// console.log(merge_arrays([2,4,3], [5,7,6]));
+
+function twoSum (nums, target){
+    for(let i = 0; i < nums.length; i++){
+        for(let j = nums.length - 1; j > i ; j--){
+            if(nums[i] + nums[j] == target){
+                return [i, j]
+            }
+        }
+    }
+    return []
+}
+// console.time("twoSum")
+// console.log(twoSum([3,2,4,5,7], 9));
+// console.timeEnd("twoSum");
+
+function addTwoNumbers(l1, l2){
+    l1 = l1.toString().replace(/,/gi, '')
+    l2 = l2.toString().replace(/,/gi, '')
+    let sum = +l1 + +l2
+    let res = 0
+    while(sum !== 0){
+        res *= 10
+        res += sum % 10
+        sum = Math.floor(sum / 10)
+    }
+    return Array.from(String(res), Number)
+}
+// console.log(addTwoNumbers([2,4,3], [5,6,4]));
+
+
